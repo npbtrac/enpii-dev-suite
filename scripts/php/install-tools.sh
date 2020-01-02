643a6e428cae9d941dc2436dd2218db933ddc7f2
Trac Nguyen <npbtrac@yahoo.com>
2020-01-02 15:39:38 +0700

    Repair php tools script

--- scripts/php/install-tools.sh
+++ scripts/php/install-tools.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cd ~
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+# Install composer
+RUN curl -sS "https://getcomposer.org/installer" | php
+RUN mv composer.phar /usr/local/bin/composer
+
+# https://github.com/hirak/prestissimo
+RUN composer global require hirak/prestissimo
+
+# Install wp-cli
+RUN curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+RUN chmod +x wp-cli.phar
+RUN mv wp-cli.phar /usr/local/bin/wp
